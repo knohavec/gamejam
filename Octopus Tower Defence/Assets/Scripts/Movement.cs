5542b4a6dd0b548decedeb2e86eba2409708be16
Keegan Nohavec <158983641+knohavec@users.noreply.github.com>
2024-05-13 03:48:37 -0600

    implemented fish
    added fish

--- Octopus Tower Defence/Assets/Scripts/Movement.cs
+++ Octopus Tower Defence/Assets/Scripts/Movement.cs
@@ -9,11 +9,19 @@ public class Movement : MonoBehaviour
     void Update()
     {
         // Move the enemy in the set direction
-        transform.Translate(moveDirection * speed * Time.deltaTime);
+        transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
     }
 
     public void SetMoveDirection(Vector3 direction)
     {
         moveDirection = direction;
+    }
+
+    // Optionally, you can add a method to smoothly rotate the enemy towards a target
+    public void RotateTowards(Vector3 target)
+    {
+        Vector3 direction = (target - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, Time.deltaTime * 360f);
     }
 }
