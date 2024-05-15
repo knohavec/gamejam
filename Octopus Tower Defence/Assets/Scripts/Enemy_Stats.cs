dc959fe2d0827e5a2a2167617132072ca1a41474
Keegan Nohavec <158983641+knohavec@users.noreply.github.com>
2024-05-15 01:01:36 -0600

    asdf
    asdf

--- Octopus Tower Defence/Assets/Scripts/Enemy_Stats.cs
+++ Octopus Tower Defence/Assets/Scripts/Enemy_Stats.cs
@@ -1,42 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Enemy_Stats : MonoBehaviour
 {
-
     [Header("Stats")]
-    [SerializeField] private int hit_points = 2;
+    [SerializeField] private int hitPoints = 2;
     [SerializeField] private int damage = 2;
 
-    private bool is_destroyed = false;
+    private bool isDestroyed = false;
 
-    public void TakeDamage(int dmg){
-        hit_points -= dmg;
+    public void TakeDamage(int dmg)
+    {
+        hitPoints -= dmg;
 
-        if (hit_points <=0 && !is_destroyed){
-            EnemySpawner.onEnemyDestroy.Invoke();
-            is_destroyed = true;
+        if (hitPoints <= 0 && !isDestroyed)
+        {
+            FindObjectOfType<SquareSpawningEnemySpawner>().EnemyDestroyed();
+            isDestroyed = true;
             Destroy(gameObject);
         }
-
-    }
-
-
-    
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Octopus Tower Defence/Assets/Scripts/Movement.cs
+++ Octopus Tower Defence/Assets/Scripts/Movement.cs
@@ -2,26 +2,55 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private Vector3 moveDirection; // The direction the enemy should move in
+    private Vector2 targetPosition; // The position of the target object
 
     public float speed = 5f; // Speed at which the enemy moves
 
     void Update()
     {
-        // Move the enemy in the set direction
-        transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+        // Move the enemy towards the target position
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        // Flip the sprite on the y-axis if the target is to the left
+        FlipSprite(targetPosition);
     }
 
-    public void SetMoveDirection(Vector3 direction)
+    // Flip the sprite based on the target position
+    // Flip the sprite based on the target position
+private void FlipSprite(Vector2 target)
+{
+    Vector2 direction = (target - (Vector2)transform.position).normalized;
+
+    // Flip the sprite on the y-axis if the target is to the right
+    if (direction.x > 0f)
     {
-        moveDirection = direction;
+        transform.localScale = new Vector3(-0.5f, 0.5f, 1f);
+    }
+    else
+    {
+        transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+    }
+}
+
+
+    // Find the nearest object with the "Tile" layer tag and set it as the target position
+    private void FindNearestTile()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        float nearestDistance = Mathf.Infinity;
+        foreach (GameObject tile in tiles)
+        {
+            float distance = Vector2.Distance(transform.position, tile.transform.position);
+            if (distance < nearestDistance)
+            {
+                nearestDistance = distance;
+                targetPosition = tile.transform.position;
+            }
+        }
     }
 
-    // Optionally, you can add a method to smoothly rotate the enemy towards a target
-    public void RotateTowards(Vector3 target)
+    void Start()
     {
-        Vector3 direction = (target - transform.position).normalized;
-        Quaternion lookRotation = Quaternion.LookRotation(direction);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, Time.deltaTime * 360f);
+        FindNearestTile(); // Find the nearest tile when the enemy is spawned
     }
 }
