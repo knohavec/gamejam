4a6c23eb9bf46a7220ccf1cb0834dc383e1f47bb
Keegan Nohavec <158983641+knohavec@users.noreply.github.com>
2024-05-14 19:01:09 -0600

    i did da thing

---
+++ Octopus Tower Defence/Assets/TowerShopButtonScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+
+
+using UnityEngine;
+
+public class TowerShopButtonScript : MonoBehaviour
+{
+    public GameObject sectionToToggle;
+
+    public void ToggleSectionVisibility()
+    {
+        if (sectionToToggle != null)
+        {
+            Debug.Log("TOGGLE SHOP");
+            sectionToToggle.SetActive(!sectionToToggle.activeSelf);
+        }
+    }
+}
+
